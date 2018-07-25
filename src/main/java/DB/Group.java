514bf3b8ef16db041ba856cd98a79c458e5c5fc0
Ion Bruckner <mihai.bruckner@gmail.com>
2018-07-25 03:22:35 +0300

    further testing

--- src/main/java/DB/Group.java
+++ src/main/java/DB/Group.java
@@ -1,48 +1,48 @@
-package DB;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
-public class Group {
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private String id;
-    private String name;
-    private String email;
-    private String p_hash;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getP_hash() {
-        return p_hash;
-    }
-
-    public void setP_hash(String p_hash) {
-        this.p_hash = p_hash;
-    }
-}
+//package DB;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//
+//@Entity
+//public class Group {
+//    @Id
+//    @GeneratedValue(strategy=GenerationType.AUTO)
+//    private String id;
+//    private String name;
+//    private String email;
+//    private String p_hash;
+//
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getP_hash() {
+//        return p_hash;
+//    }
+//
+//    public void setP_hash(String p_hash) {
+//        this.p_hash = p_hash;
+//    }
+//}
--- src/main/java/DB/Student.java
+++ src/main/java/DB/Student.java
@@ -5,20 +5,33 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+
 @Entity
 public class Student {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    private String s_number;
+//    private String s_number;
+    private Integer id;
     private String name;
     private String email;
-    private String p_hash;
-        public String getS_number() {
-            return s_number;
-        }
+//    private String p_hash;
 
-    public void setS_number(String s_number) {
-        this.s_number = s_number;
+
+//    public String getS_number() {
+//            return s_number;
+//        }
+//
+//    public void setS_number(String s_number) {
+//        this.s_number = s_number;
+//    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -37,11 +50,11 @@ public class Student {
         this.email = email;
     }
 
-    public String getP_hash() {
-        return p_hash;
-    }
+//    public String getP_hash() {
+//        return p_hash;
+//    }
 
-    public void setP_hash(String p_hash) {
-        this.p_hash = p_hash;
-    }
+//    public void setP_hash(String p_hash) {
+//        this.p_hash = p_hash;
+//    }
 }
