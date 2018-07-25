36262d021c197e156e2b9d558f09a3991a7d0e74
Ion Bruckner <mihai.bruckner@gmail.com>
2018-07-25 22:18:04 +0300

    Fixed

--- src/main/java/database/setup/MainController.java
+++ src/main/java/database/setup/MainController.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import database.setup.Student;
-import database.setup.StudentRepository;
 //import DB.Group;
 //import DB.GroupRepository;
 
